5d7c2d8c050d13449dbc0bada18cb287d80ce831
jose-robles2 <92705704+jose-robles2@users.noreply.github.com>
2023-09-25 22:25:50 -0700

    added shopping cart functionality

--- WebMarketPlace/src/main/java/org/example/marketplace/services/ItemServiceImpl.java
+++ WebMarketPlace/src/main/java/org/example/marketplace/services/ItemServiceImpl.java
@@ -4,13 +4,22 @@ import org.example.marketplace.entities.Item;
 import org.example.marketplace.repositories.ItemRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 @Service
 public class ItemServiceImpl implements ItemService {
 
     private final ItemRepository itemRepository;
 
-    public ItemServiceImpl(ItemRepository itemRepository) {
+    private final List<Item> shoppingCart;
+
+    private final HashMap<String, Integer> itemCountMap = new HashMap<>();
+
+    public ItemServiceImpl(ItemRepository itemRepository, List<Item> shoppingCart) {
         this.itemRepository = itemRepository;
+        this.shoppingCart = shoppingCart;
     }
 
     @Override
@@ -33,5 +42,61 @@ public class ItemServiceImpl implements ItemService {
         // Validate purchase restrictions
         item.setStock(item.getStock() - quantity);
         return item;
+    }
+
+    @Override
+    public void addItemToCart(Item item) {
+        // Don't allow for more items in cart than available stock for a certain item
+        if (itemCountMap.containsKey(item.getName()) && itemCountMap.get(item.getName()) == item.getStock() || item.getStock() == 0) {
+            System.out.println("Number of items in cart for " + item.getName() + " cannot exceed item stock amount");
+            return;
+        }
+
+        // Add the item to the cart and the itemCount map
+        shoppingCart.add(item);
+        if (!itemCountMap.containsKey(item.getName())) {
+            itemCountMap.put(item.getName(), 1);
+        }
+        else {
+            Integer count = itemCountMap.get(item.getName());
+            count += 1;
+            itemCountMap.put(item.getName(), count);
+        }
+    }
+
+    @Override
+    public int getQuantityToDeductStock(Item targetItem) {
+        int quantity = 0;
+        for (Item item : shoppingCart) {
+            if (item.getName().equals(targetItem.getName())) {
+                quantity += 1;
+            }
+        }
+        return quantity;
+    }
+
+    @Override
+    public List<Item> getShoppingCartItems() {
+        return new ArrayList<Item>(shoppingCart);
+    }
+
+    @Override
+    public int getShoppingCartSize() {
+        return shoppingCart.size();
+    }
+
+    @Override
+    public double getShoppingCartTotal() {
+        double total = 0.0;
+        for (Item item : shoppingCart) {
+            total += item.getPrice();
+        }
+        return total;
+    }
+
+    @Override
+    public void clearShoppingCart() {
+        itemCountMap.clear();
+        shoppingCart.clear();
     }
 }
